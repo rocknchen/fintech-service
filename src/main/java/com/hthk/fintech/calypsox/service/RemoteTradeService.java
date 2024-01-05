71788a278da2b3e3b2f7109c2c48eced812687f5
rockchen <rochchen@htsc.com>
2024-01-05 17:52:58 +0800

    update

--- src/main/java/com/hthk/fintech/calypsox/service/RemoteTradeService.java
+++ src/main/java/com/hthk/fintech/calypsox/service/RemoteTradeService.java
@@ -1,12 +1,21 @@
 package com.hthk.fintech.calypsox.service;
 
 import com.hthk.calypsox.model.trade.criteria.CriteriaTrade;
+import com.hthk.fintech.fintechservice.controller.FintechServiceController;
+import com.hthk.fintech.fintechservice.service.impl.RemoteServiceClientImpl;
+import com.hthk.fintech.model.data.datacenter.query.EntityTypeEnum;
 import com.hthk.fintech.model.software.app.ApplicationInstance;
 import com.hthk.fintech.model.trade.TradeInfo;
-import com.hthk.fintech.model.web.http.RequestDateTime;
+import com.hthk.fintech.model.web.http.*;
+import com.hthk.fintech.structure.utils.JacksonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static com.hthk.fintech.config.FintechStaticData.LOG_WRAP;
 
 /**
  * @Author: Rock CHEN
@@ -15,14 +24,43 @@ import java.util.List;
 @Service
 public class RemoteTradeService {
 
+    private final static Logger logger = LoggerFactory.getLogger(RemoteTradeService.class);
+
+    private RemoteServiceClientImpl client = new RemoteServiceClientImpl();
+
     /**
      * call remote service client
+     *
      * @param source
      * @param dateTime
      * @param criteria
      * @return
      */
     public List<TradeInfo> getTrade(ApplicationInstance source, RequestDateTime dateTime, CriteriaTrade criteria) {
+
+        HttpServiceRequest request = new HttpServiceRequest();
+        IRequestAction<HttpRequestGetParams> action = new IRequestAction<>();
+        action.setName(ActionTypeEnum.GET);
+        HttpRequestGetParams params = new HttpRequestGetParams();
+        params.setSource(source);
+        action.setParams(params);
+        request.setAction(action);
+
+        request.setDateTime(dateTime);
+
+        RequestEntity requestEntity = new RequestEntity();
+        request.setEntity(requestEntity);
+        requestEntity.setType(EntityTypeEnum.TRADE);
+        requestEntity.setSubType1("NA");
+        requestEntity.setSubType2("NA");
+        requestEntity.setSubType3("NA");
+        requestEntity.setSubType4("NA");
+        requestEntity.setSubType5("NA");
+
+        request.setCriteria(criteria);
+
+        logger.info(LOG_WRAP, "response", JacksonUtils.toJsonPrettyTry(client.call(request, HttpResponse.class)));
+
         return null;
     }
 
--- src/test/java/com/hthk/fintech/calypsox/service/RemoteTradeServiceTest.java
+++ src/test/java/com/hthk/fintech/calypsox/service/RemoteTradeServiceTest.java
@@ -1,6 +1,14 @@
 package com.hthk.fintech.calypsox.service;
 
+import com.hthk.calypsox.model.trade.criteria.CriteriaTrade;
+import com.hthk.fintech.model.data.datacenter.query.EntityTypeEnum;
+import com.hthk.fintech.model.software.app.ApplicationEnum;
+import com.hthk.fintech.model.software.app.ApplicationInstance;
+import com.hthk.fintech.model.web.http.*;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -10,7 +18,27 @@ import static org.junit.Assert.*;
  */
 public class RemoteTradeServiceTest {
 
+    RemoteTradeService remoteTradeService = new RemoteTradeService();
+
     @Before
     public void setUp() throws Exception {
     }
+
+    @Test
+    public void testGetTrade_BY_TRADEFILTER() {
+
+        ApplicationInstance instance = new ApplicationInstance();
+        instance.setName(ApplicationEnum.CALYPSO);
+
+        RequestDateTime dateTime = new RequestDateTime();
+        dateTime.setTimeZone("HKT");
+        dateTime.setRunDateTime("2023-12-20 14:19:20");
+
+        CriteriaTrade criteria = new CriteriaTrade();
+        criteria.setBookList(Arrays.asList("CIFXDH"));
+        criteria.setTradeFilter("HTHK_FICC_TEST");
+
+        remoteTradeService.getTrade(instance, dateTime, criteria);
+    }
+
 }

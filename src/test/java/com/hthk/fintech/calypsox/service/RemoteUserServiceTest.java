965fbd4b50b1398ce66fd1458e4d75b95356e51d
rockchen <rochchen@htsc.com>
2024-04-17 16:07:25 +0800

    update

--- src/test/java/com/hthk/fintech/calypsox/service/RemoteUserServiceTest.java
+++ src/test/java/com/hthk/fintech/calypsox/service/RemoteUserServiceTest.java
@@ -4,11 +4,16 @@ import com.hthk.calypsox.model.person.UserInfo;
 import com.hthk.calypsox.model.person.UserInfoVO;
 import com.hthk.calypsox.model.person.criteria.CriteriaUser;
 import com.hthk.calypsox.model.staticdata.book.BookAccess;
+import com.hthk.calypsox.model.staticdata.book.BookAccessALl;
 import com.hthk.calypsox.model.staticdata.book.BookAccessOrig;
 import com.hthk.calypsox.model.staticdata.book.BookTestInfo;
+import com.hthk.calypsox.model.staticdata.book.criteria.CriteriaBook;
+import com.hthk.common.utils.FileUtils;
 import com.hthk.fintech.exception.ServiceInternalException;
+import com.hthk.fintech.fintechservice.comparator.BasicBookInfoComparator;
 import com.hthk.fintech.model.software.app.ApplicationEnum;
 import com.hthk.fintech.model.software.app.ApplicationInstance;
+import com.hthk.fintech.model.staticdata.BookInfo;
 import com.hthk.fintech.model.web.http.RequestDateTime;
 import com.hthk.fintech.structure.utils.JacksonUtils;
 import com.hthk.fintech.utils.CSVUtils;
@@ -20,8 +25,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeansException;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
@@ -40,10 +45,16 @@ public class RemoteUserServiceTest {
 
     RemoteUserService remoteUserService = new RemoteUserService();
 
+    RemoteBookService remoteBookService = new RemoteBookService();
+
     List<BookTestInfo> bookTestInfoList = new ArrayList<>();
+
+    List<String> ficcGroupList = new ArrayList<>();
 
     @Before
     public void setUp() throws Exception {
+
+        String ficcBookFile = "C:/Rock/Datas/Docs/HTS/jira/cal1345/ficc_books.csv";
 
         int num = 1;
         bookTestInfoList.add(new BookTestInfo(num++, "CIFXDH", "FX Option"));
@@ -71,10 +82,13 @@ public class RemoteUserServiceTest {
         bookTestInfoList.add(new BookTestInfo(num++, "CIPNCOMM", "Passthrough"));
         bookTestInfoList.add(new BookTestInfo(num++, "CIPNFX", "Passthrough"));
 
+        ficcGroupList.addAll(FileUtils.readResourceAsStrList(new File(ficcBookFile)));
+
     }
 
     public RemoteUserServiceTest() {
         RemoteServiceUtils.setup(remoteUserService);
+        RemoteServiceUtils.setup(remoteBookService);
     }
 
     @Test
@@ -127,6 +141,112 @@ public class RemoteUserServiceTest {
         List<UserInfoVO> voList = convert(userInfoList);
 
         CSVUtils.write(voList, outputFile, "UTF-8", true, UserInfoVO.class);
+    }
+
+    @Test
+    public void testGetAccessOfFICCBooks() throws IOException, NoSuchFieldException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, ServiceInternalException {
+
+        String outputFile = "C:/Rock/Datas/IT/DEV_Datas/tmp/allFICCUserAccess.csv";
+
+        String instanceName = ENV_NAME_UAT;
+
+        List<BookInfo> ficcBookList = getFICCBookList(instanceName);
+        logger.info("ficcBookList: {}", JacksonUtils.toYMLPrettyTry(ficcBookList.get(0)));
+        logger.info("count: {}", ficcBookList.size());
+        Collections.sort(ficcBookList, new BasicBookInfoComparator());
+
+        List<UserInfo> userInfoList = getALlFICCUserList(instanceName, ficcGroupList);
+        logger.info("userInfoList: {}", userInfoList.size());
+
+        List<BookAccessALl> bookAccessALlList = generate(ficcBookList, userInfoList);
+
+        CSVUtils.write(bookAccessALlList, outputFile, "UTF-8", true, UserInfoVO.class);
+    }
+
+    private List<BookAccessALl> generate(List<BookInfo> ficcBookList, List<UserInfo> userInfoList) {
+        List<BookAccessALl> bookAccessALlList = new ArrayList<>();
+        ficcBookList.forEach(t -> bookAccessALlList.addAll(generate(t, userInfoList)));
+        return bookAccessALlList;
+    }
+
+    private List<BookAccessALl> generate(BookInfo bookInfo, List<UserInfo> userInfoList) {
+        List<BookAccessALl> list = userInfoList.stream().map(ui -> generate(bookInfo, ui)).collect(Collectors.toList());
+        return list.stream().filter(t -> t != null).collect(Collectors.toList());
+    }
+
+    private BookAccessALl generate(BookInfo bookInfo, UserInfo ui) {
+        List<String> bookReadWriteList = ui.getBookReadWrite();
+        List<String> bookReadOnlyList = ui.getBookReadOnly();
+        String bookName = bookInfo.getName();
+        if (bookReadWriteList.contains(bookName)) {
+            return generateBookAccessAll(bookInfo, ui, "Read and Write");
+        } else if (bookReadOnlyList.contains(bookName)) {
+            return generateBookAccessAll(bookInfo, ui, "ReadOnly");
+        } else {
+            return null;
+        }
+    }
+
+    private BookAccessALl generateBookAccessAll(BookInfo bookInfo, UserInfo ui, String accessType) {
+
+        BookAccessALl bookAccess = new BookAccessALl();
+        BeanUtils.copyProperties(bookInfo, bookAccess);
+        bookAccess.setAccessType(accessType);
+        bookAccess.setUserName(ui.getFullName());
+        return bookAccess;
+    }
+
+    private List<UserInfo> getALlFICCUserList(String instanceName, List<String> ficcGroupList) throws ServiceInternalException {
+
+        ApplicationInstance instance = new ApplicationInstance();
+        instance.setName(ApplicationEnum.CALYPSO);
+        instance.setInstance(instanceName);
+
+        RequestDateTime dateTime = new RequestDateTime();
+        dateTime.setTimeZone("HKT");
+        dateTime.setRunDateTime("2023-12-20 14:19:20");
+
+        CriteriaUser criteria = new CriteriaUser();
+
+        List<UserInfo> userInfoList = remoteUserService.getUserInfoList(instance, dateTime, criteria);
+        logger.info("All user: {}", userInfoList.size());
+
+        List<UserInfo> ficcUserList = filter(userInfoList, ficcGroupList);
+        logger.info("FICC user: {}", ficcUserList.size());
+
+        return ficcUserList;
+    }
+
+    private List<UserInfo> filter(List<UserInfo> userInfoList, List<String> ficcGroupList) {
+        return userInfoList.stream().filter(user -> isIn(user.getGroupList(), ficcGroupList)).collect(Collectors.toList());
+    }
+
+    private boolean isIn(List<String> userGroupList, List<String> ficcGroupList) {
+        boolean isIn = false;
+        for (int i = 0; i < userGroupList.size(); i++) {
+            if (ficcGroupList.contains(userGroupList.get(i))) {
+                isIn = true;
+            }
+        }
+        return isIn;
+    }
+
+    private List<BookInfo> getFICCBookList(String instanceName) throws ServiceInternalException {
+
+        ApplicationInstance instance = new ApplicationInstance();
+        instance.setName(ApplicationEnum.CALYPSO);
+        instance.setInstance(instanceName);
+
+        RequestDateTime dateTime = new RequestDateTime();
+        dateTime.setTimeZone("HKT");
+        dateTime.setRunDateTime("2023-12-20 14:19:20");
+
+        CriteriaBook criteria = new CriteriaBook();
+        criteria.setLegalEntityList(Arrays.asList("HKFH", "HKCI", "HTIFP"));
+        criteria.setCompanyShortNameList(Arrays.asList("FICC", "TRES"));
+        List<BookInfo> bookInfoList = remoteBookService.getBook(instance, dateTime, criteria);
+
+        return bookInfoList;
     }
 
     private UserInfoVO convert(UserInfo userInfo, String groupName) {
